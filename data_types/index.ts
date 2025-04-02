b25440cd1f3af7c1be80277e543722e8a242b445
npesaras <nilmar.pesarast@gmail.com>
2025-04-02 20:59:32 +0800

    Init

---
+++ data_types/index.ts
@@ -0,0 +1,27 @@
+/*
+Introduction to Data Types
+
+Data types are used to define the type of data that a variable can hold.
+
+Primitive Types
+- string
+- number
+- boolean
+- null
+- undefined
+
+Objects
+- interface
+- type
+- class
+
+*/
+
+//Primitive Types
+let stringVar: string = "Hello";           // strings
+let numberVar: number = 42;                // numbers (includes integers and floats)
+let booleanVar: boolean = true;            // booleans
+let nullVar: null = null;                  // null
+let undefinedVar: undefined = undefined;   // undefined
+let symbolVar: symbol = Symbol("sym");     // symbols
+let bigIntVar: bigint = BigInt(9007199254740991); // big integers
